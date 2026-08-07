5f8db3b32274a623dcd6f9e86a909cffb88d2764
fellow-Git-user <martynas.nadeika@gmail.com>
2026-08-07 23:55:45 +0300

    input adds to the correct place

--- JS/wordle.js
+++ JS/wordle.js
@@ -2,13 +2,10 @@ const API_URL = 'https://words.dev-apis.com/word-of-the-day'
 const WORD_VALIDATION = 'https://words.dev-apis.com/validate-word'
 
 const inputElement = document.querySelector("input")
-const inputDisplay = document.getElementById("input-display")
+const inputDisplay = document.getElementsByClassName("input-display")
 const isSpan = document.querySelectorAll("span")
-// console.log("🚀 ~ isSpan:", isSpan)
+let clickCount = 0;
 
-// isSpan.forEach((element, index) => {
-//     console.log(isSpan[index] = element.textContent + 'baa')
-// })
 
 async function getWordOfTheDay () {
     try {
@@ -28,13 +25,13 @@ async function getWordOfTheDay () {
 }
 
 function keyboardClick (event) {
-    if (isLetter(event) && inputDisplay.textContent.length < 5) {
-        for(i = 0; i < 5; i++) {
-            isSpan.forEach((element, index) => {
-                isSpan[index] = element.textContent + event;
-                console.log("🚀 ~ keyboardClick ~ isSpan[index] = element.textContent + event;:", isSpan[index] = element.textContent + event)
-            })
-        }
+    if (isLetter(event) && clickCount < 25) {
+        isSpan.forEach((element, index) => {
+            console.log(index, element)
+            isSpan[clickCount].textContent = event 
+
+        })
+
         // inputDisplay.textContent += event
         // console.log("🚀 ~ keyboardClick ~ inputDisplay.textContent.length:", inputDisplay.textContent.length)
         // console.log("veikia", event)
@@ -50,6 +47,8 @@ function isLetter(letter) {
 function init () {
     inputElement.addEventListener("keydown", function (event) {
         keyboardClick(event.key);
+        clickCount++
+        console.log("🚀 ~ clickCount:", clickCount)
     })
 }
 
