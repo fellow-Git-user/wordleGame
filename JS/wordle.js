28e33b8d307cf3958c1c0ef876877dac34f29849
fellow-Git-user <martynas.nadeika@gmail.com>
2026-08-11 13:37:42 +0300

    jump to next row after hitting enter - done, backspace functionality also implemented

--- JS/wordle.js
+++ JS/wordle.js
@@ -3,19 +3,35 @@ const loadingDiv = document.querySelector('.info-bar');
 const ANSWER_LENGTH = 5;
 
 
+
 async function init() {
     let currentGuess = '';
+    let currentRow = 0;
 
     function addLetter(letter) {
         if (currentGuess.length < ANSWER_LENGTH) {
             currentGuess += letter;
         } else {
+            // replace the last letter
             currentGuess = currentGuess.substring(0, currentGuess.length - 1)  + letter ;
         }
 
-        letters[currentGuess.length - 1].innerText = letter;
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
     }
     
+    async function commit() {
+        if (currentGuess.length !== ANSWER_LENGTH) {
+            //do nothing
+            return;
+        }
+        currentRow++;
+        currentGuess = '';
+    }
+
+    function backspace() {
+        currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
+    }
 
     document.addEventListener('keydown', function handleKeyPress(event) {
         const action = event.key;
