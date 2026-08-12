cd4c0d5a2f2f21d1cfa57d201bf9af4b92fcb807
fellow-Git-user <martynas.nadeika@gmail.com>
2026-08-12 23:05:10 +0300

    fetch word of the day, loading state

--- JS/wordle.js
+++ JS/wordle.js
@@ -8,6 +8,11 @@ async function init() {
     let currentGuess = '';
     let currentRow = 0;
 
+    const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+    const resObj = await res.json();
+    const word = resObj.word.toUpperCase();
+    setLoading(false)
+
     function addLetter(letter) {
         if (currentGuess.length < ANSWER_LENGTH) {
             currentGuess += letter;
@@ -52,6 +57,10 @@ function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
-
+function setLoading(isLoading) {
+    //if it is loading, it will remove "hidden" class
+    //if false  it will add it
+    loadingDiv.classList.toggle('hidden', !isLoading)
+}
 
 init();
